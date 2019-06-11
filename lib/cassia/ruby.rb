e7bf4488ee91bb96943edd1086f6df009b7fa689
adelinewang679 <adelinewang679@gmail.com>
2019-06-11 11:03:36 -0400

    initial commit

---
+++ lib/cassia/ruby.rb
@@ -0,0 +1,8 @@
+require "cassia/ruby/version"
+
+module Cassia
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cassia/ruby/version.rb
@@ -0,0 +1,5 @@
+module Cassia
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
