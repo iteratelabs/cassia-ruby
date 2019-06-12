eb499413951ccc93729b583e5f87461831325fb4
adelinewang679 <adelinewang679@gmail.com>
2019-06-12 15:45:31 -0400

    Filter credentials out; Add endlines

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,4 +2,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :faraday
   c.configure_rspec_metadata!
+  c.filter_sensitive_data('<CREDENTIALS>') do
+    Base64.encode64("#{ENV['CASSIA_CLIENT_ID']}:#{ENV['CASSIA_SECRET']}").strip
+  end
 end
