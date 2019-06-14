d7869910309ddac2de51be480bc0a204aa1f561c
adelinewang679 <adelinewang679@gmail.com>
2019-06-14 11:01:39 -0400

    Replaced previously unnamed variable

--- lib/cassia/requests/get_all_routers_status.rb
+++ lib/cassia/requests/get_all_routers_status.rb
@@ -3,7 +3,7 @@ module Cassia
     class GetAllRoutersStatus
       attr_reader :access_token
 
-      def initialize(access_token = nil)
+      def initialize(access_token: nil)
         @access_token = access_token
       end
 
