b8586f74f7069f66397e4fb20991dc885fdc0310
adelinewang679 <adelinewang679@gmail.com>
2019-06-14 10:45:44 -0400

    Get all routers' status
    Added tests

---
+++ lib/cassia/requests/get_all_routers_status.rb
@@ -0,0 +1,32 @@
+module Cassia
+  module Requests
+    class GetAllRoutersStatus
+      attr_reader :access_token
+
+      def initialize(access_token = nil)
+        @access_token = access_token
+      end
+
+      def path
+        '/api/cassia/hubs'
+      end
+
+      def headers
+        {
+          'Authorization' => "Bearer #{access_token}",
+          'Content-Type' => "application/json"
+        }
+      end
+
+      def perform
+        Cassia.api.get(self)
+      end
+
+      private
+
+      def access_token
+        @access_token ||= Cassia::Requests::GetToken.new.perform().body["access_token"]
+      end
+    end
+  end
+end
