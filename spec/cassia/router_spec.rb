5381492e8a518864e5473cea5ec944dd0f1e78f3
adelinewang679 <adelinewang679@gmail.com>
2019-07-01 16:40:15 -0400

    Actually Called router methods in testing router.rb(wasn't before)
    Update connected_devices list with newly created devices

--- spec/cassia/router_spec.rb
+++ spec/cassia/router_spec.rb
@@ -11,9 +11,8 @@ RSpec.describe Cassia::Router do
           Cassia.configuration.secret = ENV['CASSIA_SECRET']
           access_controller = Cassia::AccessController.new
           router = described_class.new(mac: "CC:1B:E0:E0:F1:E8")
-          request = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", type: "random")
-          
-          response = request.perform
+
+          router.connect_local(access_controller, device_mac: "F6:12:3D:BD:DE:44", type: "random")
 
           expect(router.connected_devices).not_to be_nil
           expect(access_controller.connected_devices).not_to be_nil
@@ -28,9 +27,8 @@ RSpec.describe Cassia::Router do
           access_controller = described_class.new
           access_controller = Cassia::AccessController.new
           router = described_class.new(mac: "CC:1B:E0:E0:F1:E8")
-          request = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "invalid_device_mac", type: "random")
-          
-          response = request.perform
+
+          router.connect_local(access_controller, device_mac: "invalid", type: "random")
 
           expect(access_controller.error). to eq "parameter invalid"
         end
@@ -47,9 +45,8 @@ RSpec.describe Cassia::Router do
         router = described_class.new(mac: "CC:1B:E0:E0:F1:E8")
         connect_req = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", type: "random")
         connect_res = connect_req.perform
-        request = Cassia::Requests::DisconnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44")
         
-        response = request.perform
+        router.disconnect_local(access_controller, device_mac: "F6:12:3D:BD:DE:44" )
 
         expect(router.connected_devices).to eq []
         expect(access_controller.connected_devices).to eq []
@@ -66,8 +63,8 @@ RSpec.describe Cassia::Router do
         router = described_class.new(mac: "CC:1B:E0:E0:F1:E8")
         connect_req = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", type: "random")
         connect_res = connect_req.perform
-        request = Cassia::Requests::DisconnectLocal.new(access_controller, router: router, device_mac: "invalid_device_mac")          
-        response = request.perform
+
+        router.disconnect_local(access_controller, device_mac: "invalid_device_mac")
 
         expect(access_controller.error). to eq "parameter invalid"
       end
@@ -84,11 +81,10 @@ RSpec.describe Cassia::Router do
         router = described_class.new(mac: "CC:1B:E0:E0:F1:E8")
         connect_req = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", type: "random")
         connect_res = connect_req.perform
-        request = Cassia::Requests::GetConnectedDevicesRouter.new(access_controller, router: router)
-        
-        response = request.perform
 
-        expect(router.connected_devices[0]["id"]).to eq "F6:12:3D:BD:DE:44"
+        router.get_connected_devices(access_controller)
+
+        expect(router.connected_devices[0].mac).to eq "F6:12:3D:BD:DE:44"
       end
     end
 
@@ -99,8 +95,8 @@ RSpec.describe Cassia::Router do
         Cassia.configuration.secret = ENV['CASSIA_SECRET']
         access_controller = Cassia::AccessController.new
         router = described_class.new(mac: "invalid router mac")
-        request = Cassia::Requests::GetConnectedDevicesRouter.new(access_controller, router: router)          
-        response = request.perform
+        
+        router.get_connected_devices(access_controller)
 
         expect(access_controller.error). to eq "router's mac is invalid"
       end
