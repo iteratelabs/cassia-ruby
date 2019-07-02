56fb8f907b38970714203039027f23ca943f48e8
adelinewang679 <adelinewang679@gmail.com>
2019-07-02 11:41:20 -0400

    Allow users to open notify channel on AC
    -Added tests

---
+++ spec/cassia/response_handlers/open_notify_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe Cassia::ResponseHandlers::OpenNotify do
+  include FaradayHelpers
+  describe "#handle" do
+    it "returns true for a successful response" do
+      access_controller = Cassia::AccessController.new
+      response_body = '{"status":"accepted"}'
+      response = build_response(status: 200, body: response_body)
+      response_handler = described_class.new(access_controller, aps: ["CC:1B:E0:E0:ED:AC", "CC:1B:E0:E0:F1:E8"])
+      
+      result = response_handler.handle(response)
+
+      expect(result). to be_truthy
+    end
+
+    it "returns false for an unsuccessful response" do
+      access_controller = Cassia::AccessController.new
+      response_body = '{"status":"bad request","error":"invalid aps"}'
+      response = build_response(status: 400, body: response_body)
+      response_handler = described_class.new(access_controller, aps: ["CC:1B:E0:E0:ED:AC", "CC:1B:E0:E0:F1:E8"])
+      
+      result = response_handler.handle(response)
+
+      expect(result). to be_falsey
+    end
+  end
+end
