ec0a198f47cbeba76ff9afa559975b07a853dc8c
adelinewang679 <adelinewang679@gmail.com>
2019-07-03 10:49:18 -0400

    Open the connection-state monitoring of connected devices on SSE tunnel

---
+++ lib/cassia/response_handlers/open_connection_state.rb
@@ -0,0 +1,32 @@
+module Cassia
+  module ResponseHandlers
+    class OpenConnectionState
+      def initialize(access_controller, aps: )
+        @access_controller = access_controller
+        @aps = aps
+      end
+
+      def handle(response)
+        if response.success?
+          handle_success
+        else
+          handle_failure(response)
+        end
+        response.success?
+      end
+
+      private
+
+      def handle_success
+        routers_to_be_turned_on = @access_controller.routers.select {|router| @aps.include?(router.mac) }
+        routers_to_be_turned_on.each do |router|
+          router.connection_state_monitor_on = true
+        end
+      end
+
+      def handle_failure(response)
+        @access_controller.error = JSON.parse(response.body)['error']
+      end
+    end
+  end
+end
