a2d33ec852ac6911c53f51c925f855db0b4f790d
adelinewang679 <adelinewang679@gmail.com>
2019-07-10 17:20:39 -0400

    Discover All Descriptors of One Char
    -Added tests

--- spec/cassia/requests/discover_descriptor_of_char_spec.rb
+++ spec/cassia/requests/discover_descriptor_of_char_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Cassia::Requests::DiscoverDescriptorOfChar do
       access_controller = Cassia::AccessController.new
       access_controller.access_token = "2ded2d8cf3073d368fec27243a71f858e9b9231d7388e63e6d2f70852c33e66f"
       router = Cassia::Router.new(mac: "CC:1B:E0:E0:F1:E8")
-      request = described_class.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", service_uuid: "00001800-0000-1000-8000-00805f9b34fb")
+      request = described_class.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", char_uuid: "00001800-0000-1000-8000-00805f9b34fb")
 
       expect(request.headers).to eq(
         {
@@ -26,8 +26,8 @@ RSpec.describe Cassia::Requests::DiscoverDescriptorOfChar do
   end
 
   describe '#perform' do
-    vcr_options = { cassette_name: 'discover_char_of_service/success', record: :new_episodes }
-      context "when passing in valid router, device_mac, and service_uuid", vcr: vcr_options do
+    vcr_options = { cassette_name: 'discover_descriptor_of_char/success', record: :new_episodes }
+      context "when passing in valid router, device_mac, and char_uuid", vcr: vcr_options do
         it "returns true" do
           Cassia.configuration.client_id = ENV['CASSIA_CLIENT_ID']
           Cassia.configuration.secret = ENV['CASSIA_SECRET']
@@ -35,9 +35,9 @@ RSpec.describe Cassia::Requests::DiscoverDescriptorOfChar do
           router = Cassia::Router.new(mac: "CC:1B:E0:E0:F1:E8")
           connect_req = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", type: "random")
           connect_res = connect_req.perform
-          service_req = Cassia::Requests::DiscoverAllServices.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44")
-          service_res = service_req.perform
-          request = described_class.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", service_uuid: "00001800-0000-1000-8000-00805f9b34fb")
+          char_req = Cassia::Requests::DiscoverAllChar.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44")
+          char_res = char_req.perform
+          request = described_class.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", char_uuid: "00002a00-0000-1000-8000-00805f9b34fb")
          
           response = request.perform
           
@@ -45,8 +45,8 @@ RSpec.describe Cassia::Requests::DiscoverDescriptorOfChar do
         end
       end
 
-    vcr_options = { cassette_name: 'discover_char_of_service/failure', record: :new_episodes }
-      context "when passing in an invalid service_uuid", vcr: vcr_options do
+    vcr_options = { cassette_name: 'discover_descriptor_of_char/failure', record: :new_episodes }
+      context "when passing in an invalid char_uuid", vcr: vcr_options do
         it "returns false" do
           Cassia.configuration.client_id = ENV['CASSIA_CLIENT_ID']
           Cassia.configuration.secret = ENV['CASSIA_SECRET']
@@ -54,9 +54,9 @@ RSpec.describe Cassia::Requests::DiscoverDescriptorOfChar do
           router = Cassia::Router.new(mac: "CC:1B:E0:E0:F1:E8")
           connect_req = Cassia::Requests::ConnectLocal.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", type: "random")
           connect_res = connect_req.perform
-          service_req = Cassia::Requests::DiscoverAllServices.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44")
-          service_res = service_req.perform
-          request = described_class.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", service_uuid: "11001800-0000-1000-8000-00805f9b34fb")          
+          char_req = Cassia::Requests::DiscoverAllChar.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44")
+          char_res = char_req.perform
+          request = described_class.new(access_controller, router: router, device_mac: "F6:12:3D:BD:DE:44", char_uuid: "11002a00-0000-1000-8000-00805f9b34fb")          
           
           response = request.perform
 
