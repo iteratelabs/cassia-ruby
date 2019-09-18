3080e68febc1e20b184b72393274a3a4846580f8
jrusso1020 <james.russo@iteratelabs.co>
2019-09-18 15:39:53 -0400

    Add in SSL capabilities
    Allow user's to use SSL when communicating with their access controller.

--- lib/cassia/configuration.rb
+++ lib/cassia/configuration.rb
@@ -1,7 +1,7 @@
 module Cassia
   class Configuration
-    attr_accessor :client_id, :secret, :ac_url
- 
+    attr_accessor :client_id, :secret, :ac_url, :client_cert, :client_key, :ca_file
+
     def ac_url
     	@ac_url ||= "http://demo.cassia.pro"
     end
